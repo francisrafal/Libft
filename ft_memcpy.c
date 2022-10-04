e55bfcdd81be816cbaf9397fc1fbbc7078839073
Francis Rafal <francis.rafal@gmail.com>
2022-10-04 17:24:03 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,1 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 17:07:30 by frafal            #+#    #+#             */
+/*   Updated: 2022/10/04 17:21:54 by frafal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "libft.h"
+
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	if ((dest == NULL) || (src == NULL))
+		return (NULL);
+	char	*d;
+	char	*s;
+
+	d = (char *) dest;
+	s = (char *) src;
+	while (n > 0)
+	{
+		d[n - 1] = s[n - 1];
+		n--;
+	}
+	return (dest);
+}
