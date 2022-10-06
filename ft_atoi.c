0430699cfc624212d4cf59b1146ed4c0af988011
Francis Rafal <francis.rafal@gmail.com>
2022-10-06 16:31:13 +0200

    Working ft_atoi And ft_strncmp

--- ft_atoi.c
+++ ft_atoi.c
@@ -5,9 +5,63 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/04 10:14:16 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/04 10:14:25 by frafal           ###   ########.fr       */
+/*   Created: 2022/10/06 15:14:08 by frafal            #+#    #+#             */
+/*   Updated: 2022/10/06 16:29:07 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_atoi(const char *nptr)
+{
+	long long 			i;
+	int					sign;
+	int					digits;
+	unsigned long long 	nb;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	digits = 0;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	if (nptr[i] == '+')
+		i++;
+	while (nptr[i] != '\0' && ft_isdigit(nptr[i]))
+	{
+		nb = nb * 10 + (nptr[i] - 48);
+		i++;
+		digits++;
+	}
+	if (sign == 1 && digits >= 19 && (nptr[i - digits + 18] > '7'))
+		return (-1);
+	if (sign == -1 && digits >= 19 && (nptr[i - digits + 18] > '8'))
+		return (0);
+	return (nb * sign);
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int	main(int ac, char **av)
+{
+	int	x;
+	int	y;
+	char	*nptr = av[1];
+	if (ac == 2)
+	{
+		x = ft_atoi(av[1]);
+		printf("Input:  %s\n", av[1]);
+		printf("Output mine:  %d\n", x);
+	
+		y = atoi(nptr);
+		printf("Input:  %s\n", nptr);
+		printf("Output atoi:  %d\n", y);
+	}
+	return (0);
+}
+*/
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,12 +6,12 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 14:23:41 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/04 15:24:06 by frafal           ###   ########.fr       */
+/*   Updated: 2022/10/06 12:58:17 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
@@ -21,12 +21,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (n == 0)
 		return (0);
 	i = 0;
-	c1 = s1[i];
-	c2 = s2[i];
+	c1 = (unsigned char)s1[i];
+	c2 = (unsigned char)s2[i];
 	while (s1[i] != '\0' && i < n)
 	{
-		c1 = s1[i];
-		c2 = s2[i];
+		c1 = (unsigned char)s1[i];
+		c2 = (unsigned char)s2[i];
 		if (c1 != c2)
 			return (c1 - c2);
 		i++;
@@ -34,32 +34,4 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (c1 == '\0')
 		return (c1 - c2);
 	return (0);
-}
-
-*/
-
-int ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-    size_t    i;
-
-    i = 0;
-    while (s1[i] && (i < n) && s2[i])
-    {
-        if (s1[i] == s2[i])
-        {
-            i++;
-        }
-        else if ((s1[i] - s2[i]) > 0)
-            return (1);
-        else if ((s1[i] - s2[i]) < 0)
-            return (-1);
-    }
-    if (i < n)
-    {
-        if ((s1[i] - s2[i]) > 0)
-            return (1);
-        if ((s1[i] - s2[i]) < 0)
-            return (-1);
-    }
-    return (0);
 }
