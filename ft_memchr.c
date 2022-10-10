77fcfaadb418221a8060491dd3301fdd336b0b7b
Francis Rafal <francis.rafal@gmail.com>
2022-10-10 12:33:31 +0200

    Add ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 10:52:33 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/10 10:59:32 by frafal           ###   ########.fr       */
+/*   Updated: 2022/10/10 12:32:16 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+	size_t	i;
 
+	i = 0;
+	while (i < n)
+	{
+		if (*((unsigned char*)s + i) == (unsigned char)c)
+			return ((void *)s + i);
+		i++;
+	}
+	return (NULL);
 }
