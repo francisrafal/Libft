0298839105e99ac02def9ebc04dcfaae9227ee7a
Francis Rafal <francis.rafal@gmail.com>
2022-10-10 13:08:41 +0200

    Add ft_memcmp And Refactor ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 14:23:41 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/06 12:58:17 by frafal           ###   ########.fr       */
+/*   Updated: 2022/10/10 13:07:11 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,21 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
-	int		c1;
-	int		c2;
+	unsigned char*	c1;
+	unsigned char*	c2;
 
 	if (n == 0)
 		return (0);
+	c1 = (unsigned char*)s1;
+	c2 = (unsigned char*)s2;
 	i = 0;
-	c1 = (unsigned char)s1[i];
-	c2 = (unsigned char)s2[i];
-	while (s1[i] != '\0' && i < n)
+	if (c1[i] == '\0')
+		return (c1[i] - c2[i]);
+	while (c1[i] != '\0' && i < n)
 	{
-		c1 = (unsigned char)s1[i];
-		c2 = (unsigned char)s2[i];
-		if (c1 != c2)
-			return (c1 - c2);
+		if (c1[i] != c2[i])
+			return (c1[i] - c2[i]);
 		i++;
 	}
-	if (c1 == '\0')
-		return (c1 - c2);
 	return (0);
 }
