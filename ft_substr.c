fbce2cc39ccd3ee5f3c1e6a91dde733bee5bf951
Francis Rafal <francis.rafal@gmail.com>
2022-10-10 16:48:11 +0200

    Add ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,1 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/10 15:30:40 by frafal            #+#    #+#             */
+/*   Updated: 2022/10/10 16:42:39 by frafal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	substr_len;
+
+	if (s == NULL)
+		return (NULL);
+	substr_len = 0;
+	while (s[substr_len + start] && substr_len < len)
+		substr_len++;
+	substr = (char *)malloc((substr_len + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		substr[0] = '\0';
+		return (substr);
+	}
+	ft_memmove(substr, s + start, substr_len + 1);
+	substr[substr_len] = 0;
+	return (substr);
+}
