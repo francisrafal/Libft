0590cebdaedd9dcc7b77b03d1eec9295e24e2cbc
Francis Rafal <francis.rafal@gmail.com>
2022-10-12 17:37:43 +0200

    Fix Moulinette ft_strtrim Error

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 11:28:52 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/12 17:08:53 by frafal           ###   ########.fr       */
+/*   Updated: 2022/10/12 17:36:46 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (s1[i] && in_set(s1[i], set))
 		i++;
 	len = ft_strlen(s1);
-	if (i == len && !i)
-		return ((char *)s1 + i);
-	while (len > 0 && in_set(s1[len - 1], set))
+	while (len > 0 && len > i && in_set(s1[len - 1], set))
 		len--;
 	trim = (char *)malloc((len - i + 1) * sizeof(char));
 	if (trim == NULL)
