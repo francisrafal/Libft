9cf413eaa7ce8160609c66a9a972a3c151a84892
Francis Rafal <francis.rafal@gmail.com>
2022-10-12 18:06:40 +0200

    Cast c to char in ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 17:45:47 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/06 18:25:17 by frafal           ###   ########.fr       */
+/*   Updated: 2022/10/12 18:02:56 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strrchr(const char *s, int c)
 	i = ft_strlen(s);
 	while (i >= 0)
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 			return ((char *)s + i);
 		if (i == 0)
 			return (NULL);
