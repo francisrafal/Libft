67b9dbca18884f8b95ab2dd6e84879001084ab2a
Francis Rafal <francis.rafal@gmail.com>
2022-10-12 17:53:21 +0200

    Protect ft_calloc Against MAX_SIZE

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 14:10:21 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/10 14:30:46 by frafal           ###   ########.fr       */
+/*   Updated: 2022/10/12 17:52:47 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*arr;
 	size_t	arr_size;
-
+	
+	if (size > 4294967294 || nmemb > 4294967294)
+		return (NULL);
 	arr_size = nmemb * size;
 	if (arr_size > INT_MAX)
 		return (NULL);
