791cf58fe631ccee569a8959d2ac3f45b2857de8
Francis Rafal <francis.rafal@gmail.com>
2022-10-13 13:02:47 +0200

    Fix Invalid Read In ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: frafal <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 15:30:40 by frafal            #+#    #+#             */
-/*   Updated: 2022/10/12 18:11:40 by frafal           ###   ########.fr       */
+/*   Updated: 2022/10/13 13:02:06 by frafal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s == NULL)
 		return (NULL);
 	substr_len = 0;
-	while (s[substr_len + start] && substr_len < len && start < ft_strlen(s))
+	while (start < ft_strlen(s) && s[substr_len + start] && substr_len < len)
 		substr_len++;
 	substr = (char *)malloc((substr_len + 1) * sizeof(char));
 	if (!substr)
@@ -34,3 +34,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	substr[substr_len] = 0;
 	return (substr);
 }
+/*
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char *str = strdup("1");
+	char *s = ft_substr(str, 42, 42000000);
+	printf("\"%s\"\n", s);
+	free(s); free(str); 
+	return (0);
+}
+*/
